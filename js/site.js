353e2c97f6514fdcc82bbb208e54ab83741a70f1
Joel Glovier <jglovier@github.com>
2015-02-26 00:17:13 -0500

    tweak blog index nav default behavior :wrench:

--- js/site.js
+++ js/site.js
@@ -2,6 +2,11 @@ $(function(){
 
   // Homepage | section.writing | article nav behavior
   // NOTE: this code is mad ghetto and in no wise to be held up as a standard of JavaScript brilliance; so sorry bout that; but It Works™
+
+  // first, hide everything except 2015 posts be default
+  $(".article-item:not(.article-2015)").removeClass("show");
+
+  // if visitor clicks on All, show all posts
   $("#articlesAllTrigger").on("click", function(event){
     event.preventDefault();
     $(".article-nav .year").removeClass("showing");
@@ -9,6 +14,7 @@ $(function(){
     $(".article-item").addClass("show");
   });
 
+  // if visitor clicks on 2015, show 2015 posts
   $("#articles2015Trigger").on("click", function(event){
     event.preventDefault();
     $(".article-nav .year").removeClass("showing");
@@ -17,6 +23,7 @@ $(function(){
     $(".article-item.article-2015").addClass("show");
   });
 
+  // if visitor clicks on 2014, show 2014 posts
   $("#articles2014Trigger").on("click", function(event){
     event.preventDefault();
     $(".article-nav .year").removeClass("showing");
@@ -25,6 +32,7 @@ $(function(){
     $(".article-item.article-2014").addClass("show");
   });
 
+  // if visitor clicks on 2013, show 2013 posts
   $("#articles2013Trigger").on("click", function(event){
     event.preventDefault();
     $(".article-nav .year").removeClass("showing");
@@ -33,6 +41,7 @@ $(function(){
     $(".article-item.article-2013").addClass("show");
   });
 
+  // if visitor clicks on 2012, show 2012 posts
   $("#articles2012Trigger").on("click", function(event){
     event.preventDefault();
     $(".article-nav .year").removeClass("showing");
@@ -41,6 +50,7 @@ $(function(){
     $(".article-item.article-2012").addClass("show");
   });
 
+  // if visitor clicks on 2011, show 2011 posts
   $("#articles2011Trigger").on("click", function(event){
     event.preventDefault();
     $(".article-nav .year").removeClass("showing");
