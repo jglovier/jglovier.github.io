2728e8cee81111af70e363d3062086f9e1a4668e
Joel Glovier <jglovier@github.com>
2017-08-12 18:59:28 -0400

    spike a pass at autohidding sidebar

--- js/site.js
+++ js/site.js
@@ -1,71 +1,41 @@
-$(function(){
+// modified from: http://jsfiddle.net/mariusc23/s6mLJ/31/
+// Medium post here: https://medium.com/@mariusc23/hide-header-on-scroll-down-show-on-scroll-up-67bbaae9a78c
 
-  // Homepage | section.writing | article nav behavior
-  // NOTE: this code is mad ghetto and in no wise to be held up as a standard of JavaScript brilliance; so sorry bout that; but It Works™
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.section-intro').outerHeight();
 
-  // first, hide everything except 2015 posts be default
-  $(".article-item:not(.article-2016)").removeClass("show");
-
-  // if visitor clicks on All, show all posts
-  $("#articlesAllTrigger").on("click", function(event){
-    event.preventDefault();
-    $(".article-nav .year").removeClass("showing");
-    $(this).addClass("showing");
-    $(".article-item").addClass("show");
-  });
-
-  // if visitor clicks on 2015, show 2015 posts
-  $("#articles2016Trigger").on("click", function(event){
-    event.preventDefault();
-    $(".article-nav .year").removeClass("showing");
-    $(this).addClass("showing");
-    $(".article-item").removeClass("show");
-    $(".article-item.article-2016").addClass("show");
-  });
-
-  // if visitor clicks on 2015, show 2015 posts
-  $("#articles2015Trigger").on("click", function(event){
-    event.preventDefault();
-    $(".article-nav .year").removeClass("showing");
-    $(this).addClass("showing");
-    $(".article-item").removeClass("show");
-    $(".article-item.article-2015").addClass("show");
-  });
-
-  // if visitor clicks on 2014, show 2014 posts
-  $("#articles2014Trigger").on("click", function(event){
-    event.preventDefault();
-    $(".article-nav .year").removeClass("showing");
-    $(this).addClass("showing");
-    $(".article-item").removeClass("show");
-    $(".article-item.article-2014").addClass("show");
-  });
-
-  // if visitor clicks on 2013, show 2013 posts
-  $("#articles2013Trigger").on("click", function(event){
-    event.preventDefault();
-    $(".article-nav .year").removeClass("showing");
-    $(this).addClass("showing");
-    $(".article-item").removeClass("show");
-    $(".article-item.article-2013").addClass("show");
-  });
-
-  // if visitor clicks on 2012, show 2012 posts
-  $("#articles2012Trigger").on("click", function(event){
-    event.preventDefault();
-    $(".article-nav .year").removeClass("showing");
-    $(this).addClass("showing");
-    $(".article-item").removeClass("show");
-    $(".article-item.article-2012").addClass("show");
-  });
-
-  // if visitor clicks on 2011, show 2011 posts
-  $("#articles2011Trigger").on("click", function(event){
-    event.preventDefault();
-    $(".article-nav .year").removeClass("showing");
-    $(this).addClass("showing");
-    $(".article-item").removeClass("show");
-    $(".article-item.article-2011").addClass("show");
-  });
-
+$(window).scroll(function(event){
+    didScroll = true;
 });
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('body').removeClass('sidebar-expanded').addClass('sidebar-collapsed');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('body').removeClass('sidebar-collapsed').addClass('sidebar-expanded');
+        }
+    }
+
+    lastScrollTop = st;
+}
