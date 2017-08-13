dffe0f7c3599b98a6dc469fdec1ce10ee2d2745e
Joel Glovier <jglovier@github.com>
2017-08-12 20:08:27 -0400

    spike new js functions

--- js/site.js
+++ js/site.js
@@ -1,41 +1,11 @@
-// modified from: http://jsfiddle.net/mariusc23/s6mLJ/31/
-// Medium post here: https://medium.com/@mariusc23/hide-header-on-scroll-down-show-on-scroll-up-67bbaae9a78c
+$(function(){
+  // Open sidebar when closed
+  $('.js-sidebar-toggle-open').click(function(){
+    $('body').removeClass('sidebar-collapsed').addClass('sidebar-expanded');
+  });
 
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('.section-intro').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
+  // Close sidebar when opened
+  $('.js-sidebar-toggle-closed').click(function(){
+    $('body').removeClass('sidebar-expanded').addClass('sidebar-closed');
+  });
 });
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('body').removeClass('sidebar-expanded').addClass('sidebar-collapsed');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('body').removeClass('sidebar-collapsed').addClass('sidebar-expanded');
-        }
-    }
-
-    lastScrollTop = st;
-}
