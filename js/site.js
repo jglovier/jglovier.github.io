a272a7919cee9a3eb72fb76f0026be627c57454a
Joel Glovier <jglovier@github.com>
2017-08-12 21:01:07 -0400

    :tada: finished the sidebar hiding animation

--- js/site.js
+++ js/site.js
@@ -1,11 +1,6 @@
 $(function(){
-  // Open sidebar when closed
-  $('.js-sidebar-toggle-open').click(function(){
-    $('body').removeClass('sidebar-collapsed').addClass('sidebar-expanded');
-  });
-
-  // Close sidebar when opened
-  $('.js-sidebar-toggle-closed').click(function(){
-    $('body').removeClass('sidebar-expanded').addClass('sidebar-closed');
+  // Toggle sidebar open and closed
+  $('.js-sidebar-toggle-btn').click(function(){
+    $('body').toggleClass('sidebar-closed');
   });
 });
